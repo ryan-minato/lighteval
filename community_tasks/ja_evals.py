b58e3a8785f0257420490c23e0160a98d081067c
Aoi <82735346+ryan-minato@users.noreply.github.com>
2024-12-20 19:15:49 +0900

    feat: add some tasks

--- community_tasks/ja_evals.py
+++ community_tasks/ja_evals.py
@@ -30,13 +30,144 @@ Author:
 """
 
 import numpy as np
-from aenum import extend_enum
+from scipy.stats import pearsonr, spearmanr
 
-from lighteval.metrics.metrics import Metrics, SampleLevelMetric
+from lighteval.metrics.metrics import CorpusLevelMetric, Metrics, SampleLevelMetricGrouping
+from lighteval.metrics.metrics_corpus import (
+    CorpusLevelTranslationMetric,
+)
+from lighteval.metrics.sample_preparator import (
+    GenerativeCorpusMetricInput,
+    GenerativePreparator,
+)
 from lighteval.metrics.utils.metric_utils import MetricCategory, MetricUseCase
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from lighteval.tasks.requests import Doc
+
+
+# NOTE: When performing translation tasks, the janome library needs to be installed.
+#       Since Japanese does not use spaces for tokenization, the janome library is required for "wakati-gaki".
+#       Please install the janome library using `pip install janome`.
+try:
+    from janome.tokenizer import Tokenizer
+    tokenizer = Tokenizer(wakati=True)
+except ImportError:
+    tokenizer = None
+
+
+class CorpusLevelJapaneseTranslationMetric(CorpusLevelTranslationMetric):
+    def __init__(self, metric_name: str):
+        super().__init__(metric_name)
+
+    def compute(self, items: list[GenerativeCorpusMetricInput]) -> float:
+        if not tokenizer:
+            raise ImportError(
+                "The janome library is required to use the CorpusLevelJapaneseTranslationMetric class."
+                "Please install it via `pip install janome`."
+            )
+
+        def wakati(item):
+            item.golds = [" ".join(tokenizer.tokenize(gold)) for gold in item.golds]
+            item.preds = [" ".join(tokenizer.tokenize(pred)) for pred in item.preds]
+            return item
+
+        items = [wakati(item) for item in items]
+        return super().compute(items)
+
+
+# Metrics
+ja_bleu = CorpusLevelMetric(
+    metric_name="bleu",
+    sample_level_fn=GenerativePreparator().prepare,
+    category=MetricCategory.GENERATIVE,
+    use_case=MetricUseCase.TRANSLATION,
+    corpus_level_fn=CorpusLevelJapaneseTranslationMetric("bleu").compute,
+    higher_is_better=True,
+)
+ja_chrf = CorpusLevelMetric(
+    metric_name="chrf",
+    sample_level_fn=GenerativePreparator().prepare,
+    category=MetricCategory.GENERATIVE,
+    use_case=MetricUseCase.TRANSLATION,
+    corpus_level_fn=CorpusLevelJapaneseTranslationMetric("chrf").compute,
+    higher_is_better=True,
+)
+ja_ter = CorpusLevelMetric(
+    metric_name="ter",
+    sample_level_fn=GenerativePreparator().prepare,
+    category=MetricCategory.GENERATIVE,
+    use_case=MetricUseCase.TRANSLATION,
+    corpus_level_fn=CorpusLevelJapaneseTranslationMetric("ter").compute,
+    higher_is_better=False,
+)
+
+
+# Metrics
+def correlation_metric(golds: list[int], predictions: list[str], **kwargs):
+    def convert_to_float(score):
+        try:
+            return float(score)
+        except ValueError:
+            return None
+
+    predicted_score = convert_to_float(predictions[0])
+    gold_score = convert_to_float(golds[0])
+
+    return {
+        "predicted_score": predicted_score,
+        "gold_score": gold_score,
+    }
+
+
+def spearman_corpus_metric(items):
+    predicted_scores, gold_scores = zip(
+        *[
+            (item["predicted_score"], item["gold_score"])
+            for item in items
+            if (item["gold_score"] is not None and item["predicted_score"] is not None)
+        ]
+    )
+    r, _ = spearmanr(predicted_scores, gold_scores)
+    if np.isnan(r):
+        return 0.0
+    frac = len(predicted_scores) / len(items)
+
+    return r * frac
+
+
+def pearson_corpus_metric(items):
+    predicted_scores, gold_scores = zip(
+        *[
+            (item["predicted_score"], item["gold_score"])
+            for item in items
+            if (item["gold_score"] is not None and item["predicted_score"] is not None)
+        ]
+    )
+    r, _ = pearsonr(predicted_scores, gold_scores)
+    if np.isnan(r):
+        return 0.0
+    frac = len(predicted_scores) / len(items)
+    return r * frac
+
+
+spearman_metric = CorpusLevelMetric(
+    metric_name="spearman_correlation",
+    higher_is_better=True,
+    category=MetricCategory.GENERATIVE,
+    use_case=MetricUseCase.NONE,
+    sample_level_fn=correlation_metric,
+    corpus_level_fn=spearman_corpus_metric,
+)
+
+pearson_metric = CorpusLevelMetric(
+    metric_name="pearson_correlation",
+    higher_is_better=True,
+    category=MetricCategory.GENERATIVE,
+    use_case=MetricUseCase.NONE,
+    sample_level_fn=correlation_metric,
+    corpus_level_fn=pearson_corpus_metric,
+)
 
 
 # NLI
@@ -480,7 +611,6 @@ CHABSA_POLARITY_PROMPT_TEMPLAT = """\
 [極性]: """
 
 
-# [ { "from": 35, "label": "general", "polarity": "positive", "text": "需要", "to": 37 }, { "from": 60, "label": "general", "polarity": "negative", "text": "着工ペース", "to": 65 }, { "from": 69, "label": "sales", "polarity": "positive", "text": "売上", "to": 71 } ]
 def chabsa_polarity_prompt_fn(line, task_name: str = None):
     entity = ",".join([f"{entity['text']}, {entity['polarity']}" for entity in line["opinions"]])
     prompt = CHABSA_POLARITY_PROMPT_TEMPLAT.format(
@@ -623,8 +753,17 @@ def build_mt_prompt_fn(
     target_lang: str,
     original_colunm_name: str | None = None,
     target_colunm_name: str | None = None,
+    cutoff_length: int = 1024,
     instruction_template: str = MT_INSTRUCTION_TEMPLATE,
 ):
+    try:
+        from janome.tokenizer import Tokenizer
+    except ImportError:
+        raise ImportError(
+            "The janome library is required to use the build_mt_prompt_fn function."
+            "Please install it via pip install janome."
+        )
+
     original_colunm_name = original_colunm_name or f"{original_lang}"
     target_colunm_name = target_colunm_name or f"{target_lang}"
     instruction = instruction_template.format(
@@ -634,8 +773,8 @@ def build_mt_prompt_fn(
 
     def mt_prompt_fn(line, task_name: str = None):
         original_text = line[original_colunm_name]
-        if len(original_text) > 2048:
-            original_text = original_text[:2048]
+        if len(original_text) > cutoff_length:
+            original_text = original_text[:cutoff_length]
 
         prompt = MT_PROMPT_TEMPLATE.format(
             original=line[original_colunm_name]
@@ -653,25 +792,278 @@ def build_mt_prompt_fn(
 
     return mt_prompt_fn
 
+
 # ALT
+def build_mt_task(
+        name: str,
+        hf_repo: str,
+        original_lang: str,
+        target_lang: str,
+        subset: str,
+        generation_size: int = 1024,
+):
+    lang_name_map = {
+        "english": "英語",
+        "japanese": "日本語",
+    }
+
+    return LightevalTaskConfig(
+        name=name,
+        prompt_function=build_mt_prompt_fn(
+            lang_name_map[original_lang], lang_name_map[target_lang], original_lang, target_lang
+            ),
+        suite=["community"],
+        hf_repo=hf_repo,
+        hf_subset=subset,
+        hf_avail_splits=["test", "train"],
+        evaluation_splits=["test"],
+        few_shots_split="train",
+        few_shots_select=None,
+        generation_size=generation_size,
+        stop_sequence=["\n"],
+        metric=[
+            ja_bleu,
+            ja_chrf,
+            ja_ter,
+        ],
+    )
 
 
-task_alt_e_to_j = LightevalTaskConfig(
-    name="ja:alt_e_to_j",
-    prompt_function=build_mt_prompt_fn("english", "japanese"),
+alt_tasks = [
+    build_mt_task("ja:alt:e_to_j", "zenless-lab/alt", "english", "japanese", "default", 4096),
+    *[
+        build_mt_task(f"ja:alt_{key}:e_to_j", "zenless-lab/alt", "english", "japanese", f"{key}-token", value)
+        for key, value in {
+            "256": 256,
+            "512": 512,
+            "1k": 1024,
+            "2k": 2048,
+        }.items()
+    ],
+    build_mt_task("ja:alt:j_to_e", "zenless-lab/alt", "japanese", "english", "default", 4096),
+    *[
+        build_mt_task(f"ja:alt_{key}:j_to_e", "zenless-lab/alt", "japanese", "english", f"{key}-token", value)
+        for key, value in {
+            "256": 256,
+            "512": 512,
+            "1k": 1024,
+            "2k": 2048,
+        }.items()
+    ],
+]
+
+
+# Semantic Text Similarity
+# JSTS
+JSTS_INSTRUCTION = (
+    "日本語の文ペアの意味がどのくらい近いかを判定し、類似度を0.0〜5.0までの間の値で付与してください。"
+    "0.0に近いほど文ペアの意味が異なり、5.0に近いほど文ペアの意味が似ていることを表しています。"
+    "整数値のみを返し、それ以外には何も含めないことを厳守してください。"
+)
+JSTS_PROMPT_TEMPLAT = """\
+[文1]: {sentence1}
+[文2]: {sentence2}
+[類似度]: """
+
+
+def jsts_prompt_fn(line, task_name: str = None):
+    prompt = JSTS_PROMPT_TEMPLAT.format(
+        sentence1=line["sentence1"],
+        sentence2=line["sentence2"]
+    )
+    query = JSTS_INSTRUCTION + "\n\n" + prompt
+    answer = line["label"]
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[answer],
+        gold_index=0,
+        instruction=JSTS_INSTRUCTION,
+    )
+
+
+jsts_task = LightevalTaskConfig(
+    name="jglue:jsts",
+    prompt_function=jsts_prompt_fn,
     suite=["community"],
-    hf_repo="zenless-lab/alt",
+    hf_repo="zenless-lab/jsts",
+    hf_subset="default",
+    hf_avail_splits=["train", "validation"],
+    evaluation_splits=["validation"],
+    few_shots_split="train",
+    few_shots_select=None,
+    generation_size=100,
+    stop_sequence=["\n"],
+    metric=[spearman_metric, pearson_metric],
+)
+
+wikicorpus_tasks = [
+    build_mt_task("ja:wikicorpus_e_to_j", "zenless-lab/wikicorpus", "english", "japanese", "default", 4096),
+    build_mt_task("ja:wikicorpus_j_to_e", "zenless-lab/wikicorpus", "japanese", "english", "default", 4096),
+]
+
+
+# Human Examination
+HE_INSTRUCTION = (
+    "与えられた質問と選択肢から、最も適切な回答を選択してください。"
+    "なお、回答には選択肢の内容をそのまま出力し、他には何も含めないことを厳守してください。"
+)
+
+HE_PROMPT_TEMPLAT = """\
+[質問]: {question}
+[選択肢]: {choices}
+[回答]: """
+
+
+def jmmlu_prompt_fn(line, task_name: str = None):
+    prompt = f"{line['question']}\n"
+    query = HE_INSTRUCTION + "\n\n" + prompt
+    choices = [line[f"choice{i}"] for i in range(4)]
+    answer = line["label"]
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=choices,
+        gold_index=answer,
+        instruction=HE_INSTRUCTION,
+    )
+
+# JMMLU
+
+
+jmmlu_task = LightevalTaskConfig(
+    name="ja:jmmlu",
+    prompt_function=jmmlu_prompt_fn,
+    suite=["community"],
+    hf_repo="zenless-lab/jmmlu",
     hf_subset="default",
     hf_avail_splits=["test", "train"],
     evaluation_splits=["test"],
     few_shots_split="train",
     few_shots_select=None,
-    generation_size=1024,
+    generation_size=100,
     stop_sequence=["\n"],
     metric=[
-        Metrics.f1_score,
-        # Metrics.bleu,
-        # Metrics.bert_score,  # TODO: Add chunked bert_score metric
+        Metrics.loglikelihood_acc,
+        Metrics.loglikelihood_acc_norm,
+        Metrics.loglikelihood_acc_norm_nospace,
+    ],
+)
+
+jmmlu_nc_nd_task = LightevalTaskConfig(
+    name="ja:jmmlu_nc_nd",
+    prompt_function=jmmlu_prompt_fn,
+    suite=["community"],
+    hf_repo="zenless-lab/jmmlu-nc-nd",
+    hf_subset="default",
+    hf_avail_splits=["test"],
+    evaluation_splits=["test"],
+    few_shots_split=None,
+    few_shots_select=None,
+    generation_size=100,
+    stop_sequence=["\n"],
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.loglikelihood_acc,
+        Metrics.loglikelihood_acc_norm,
+        Metrics.loglikelihood_acc_norm_nospace,
+    ],
+)
+
+
+# MMMLU
+def mmmlu_prompt_fn(line, task_name: str = None):
+    answer_index_map = {
+        "A": 0,
+        "B": 1,
+        "C": 2,
+        "D": 3,
+    }
+    prompt = f"{line['Question']}\n"
+    query = HE_INSTRUCTION + "\n\n" + prompt
+    choices = [line[key] for key in answer_index_map.keys()]
+    answer = answer_index_map[line["Answer"]]
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=choices,
+        gold_index=answer,
+        instruction=HE_INSTRUCTION,
+    )
+
+
+mmmlu_task = LightevalTaskConfig(
+    name="ja:mmmlu",
+    prompt_function=mmmlu_prompt_fn,
+    suite=["community"],
+    hf_repo="openai/MMMLU",
+    hf_subset="JA_JP",
+    hf_avail_splits=["test"],
+    evaluation_splits=["test"],
+    few_shots_split=None,
+    few_shots_select=None,
+    generation_size=100,
+    stop_sequence=["\n"],
+    metric=[
+        Metrics.exact_match,
+        Metrics.quasi_exact_match,
+        Metrics.loglikelihood_acc,
+        Metrics.loglikelihood_acc_norm,
+        Metrics.loglikelihood_acc_norm_nospace,
+    ],
+)
+
+# Code Generation
+# MBPP
+# TODO: Code Generation tasks(llm-jp/mbpp-ja)
+
+# Summarization
+# XL-Sum
+XL_SUM_INSTRUCTION = (
+    "与えられたニュース記事を要約してください。"
+)
+XL_SUM_PROMPT_TEMPLAT = """\
+[記事]: {article}
+[要約]: """
+
+
+def xl_sum_prompt_fn(line, task_name: str = None):
+    prompt = XL_SUM_PROMPT_TEMPLAT.format(
+        article=line["text"]
+    )
+    query = XL_SUM_INSTRUCTION + "\n\n" + prompt
+    answer = line["summary"]
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[answer],
+        gold_index=0,
+        instruction=XL_SUM_INSTRUCTION,
+    )
+
+
+xl_sum_task = LightevalTaskConfig(
+    name="ja:xl_sum",
+    prompt_function=xl_sum_prompt_fn,
+    suite=["community"],
+    hf_repo="csebuetnlp/xlsum",
+    hf_subset="japanese",
+    hf_avail_splits=["test", "train"],
+    evaluation_splits=["test"],
+    few_shots_split="train",
+    few_shots_select=None,
+    generation_size=512,
+    stop_sequence=["\n"],
+    metric=[
+        Metrics.rouge1,
+        Metrics.rouge2,
+        Metrics.rougeL,
+        ja_bleu,
     ],
 )
 
@@ -680,5 +1072,7 @@ TASKS_TABLE = [
     jamp_task, janli_task, jsick_task, jsick_stress_task, jsem_task,
     jnli_task, jsquad_task, jcommonsenseqa_task, kuci_task,
     jcommonsense_morality_task, chabsa_extract_task, chabsa_polarity_task,
-    mawps_task, mgsm_task, task_alt_e_to_j,
+    mawps_task, mgsm_task, *alt_tasks, *wikicorpus_tasks,
+    jsts_task, jmmlu_task, jmmlu_nc_nd_task, mmmlu_task,
+    xl_sum_task,
 ]
